eb96a3ddddaa6ce1697572b3876e9d4722ab47d9
Amit Kumar <amitnarayansah@gmail.com>
2019-09-04 19:54:35 +0530

    OTA Updater Support Added

--- HomeGateway/src/main.ino
+++ HomeGateway/src/main.ino
@@ -1,14 +1,24 @@
 #include <Arduino.h>
 #include "HomeServer.h"
+#include "CYN_OTA.h"
+#define VERSION 1.2 
+
 
 HomeServer home;
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
+  
   // put your setup code here, to run once:
   home.SetupHomeServer();
+  Serial.print("Version: "); Serial.println(VERSION);
+  FirmwareUpdater::UpdaterInstaller();
+
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
   home.LoopHomeServer();
+  FirmwareUpdater::UpdaterLooper();
 }
